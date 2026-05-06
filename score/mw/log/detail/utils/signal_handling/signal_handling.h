cafa286e27609db638b78261256b39143c66550f
Andrey Babanin <andrey.babanin@bmw.de>
2026-05-06 08:54:09 +0200

    Suppress unused variable warnings in SignalHandling::WithSigTermBlocked
    GIT_ORIGIN_SPP_REV_ID: aeef50cb3bcd10d6dc4aa801c80984e1c659510b

--- score/mw/log/detail/utils/signal_handling/signal_handling.h
+++ score/mw/log/detail/utils/signal_handling/signal_handling.h
@@ -46,6 +46,7 @@ class SignalHandling
     static auto WithSigTermBlocked(score::os::Signal& signal, Func&& func) noexcept -> decltype(func())
     {
         const auto block_result = PThreadBlockSigTerm(signal);
+        std::ignore = block_result;
         auto result = func();
         const auto unblock_result = PThreadUnblockSigTerm(signal);
         std::ignore = unblock_result;
