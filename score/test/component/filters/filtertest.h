e8018519fdba6cae4aa57566da4ed296d2d75f3d
rahulsasingh <rahul.sa.singh@partner.bmwgroup.com>
2026-05-21 10:01:57 +0200

    resolve CI build failures

--- score/test/component/filters/filtertest.h
+++ score/test/component/filters/filtertest.h
@@ -1,5 +1,4 @@
-/*******************************************************************************
- *
+/********************************************************************************
  * Copyright (c) 2025 Contributors to the Eclipse Foundation
  *
  * See the NOTICE file(s) distributed with this work for additional
@@ -10,9 +9,7 @@
  * https://www.apache.org/licenses/LICENSE-2.0
  *
  * SPDX-License-Identifier: Apache-2.0
- *******************************************************************************
- */
-
+ ********************************************************************************/
 #ifndef FILTERTEST_H
 #define FILTERTEST_H
 
