49f0a4a029f480e6033ab5e9e241fddff1366060
vaishnavi yendole <vaishnavi.yendole@bti.bmwgroup.com>
2026-05-28 22:07:26 +0530

    fix: Add missing copyright headers

--- score/datarouter/test/ut/ut_logging/test_unix_domain_common.cpp
+++ score/datarouter/test/ut/ut_logging/test_unix_domain_common.cpp
@@ -1,3 +1,16 @@
+/********************************************************************************
+ * Copyright (c) 2025 Contributors to the Eclipse Foundation
+ *
+ * See the NOTICE file(s) distributed with this work for additional
+ * information regarding copyright ownership.
+ *
+ * This program and the accompanying materials are made available under the
+ * terms of the Apache License Version 2.0 which is available at
+ * https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * SPDX-License-Identifier: Apache-2.0
+ ********************************************************************************/
+
 #include "score/os/mocklib/socketmock.h"
 #include "unix_domain/unix_domain_common.h"
 #include <gmock/gmock.h>
